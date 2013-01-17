a2b68034d42a46b05c2c3b9d5c1e83a6b932385f
Lorenz Meier <lm@inf.ethz.ch>
2013-01-17 23:55:09 +0100

    add limit to throttle for non-HIL flying, input manual controls to stabilization

--- apps/controllib/fixedwing.cpp
+++ apps/controllib/fixedwing.cpp
@@ -327,6 +327,15 @@ void BlockMultiModeBacksideAutopilot::update()
 		_actuators.control[CH_RDR] = _backsideAutopilot.getRudder();
 		_actuators.control[CH_THR] = _backsideAutopilot.getThrottle();
 
+		// XXX limit throttle to manual setting (safety) for initial testing
+		/* do not limit in HIL */
+		if (!_status.flag_hil_enabled) {
+			/* limit to value of manual throttle */
+			_actuators.control[CH_THR] = (_actuators.control[CH_THR] < _manual.throttle) ?
+							_actuators.control[CH_THR] : _manual.throttle;
+		}
+
+
 	} else if (_status.state_machine == SYSTEM_STATE_MANUAL) {
 
 		if (_status.manual_control_mode == VEHICLE_MANUAL_CONTROL_MODE_DIRECT) {
@@ -337,13 +346,24 @@ void BlockMultiModeBacksideAutopilot::update()
 			_actuators.control[CH_THR] = _manual.throttle;
 
 		} else if (_status.manual_control_mode == VEHICLE_MANUAL_CONTROL_MODE_SAS) {
-			_stabilization.update(
-			_ratesCmd.roll, _ratesCmd.pitch, _ratesCmd.yaw,
+
+			// XXX direct stabilized r p y
+			_stabilization.update(_manual.roll, _manual.pitch, _manual.yaw,
 			_att.rollspeed, _att.pitchspeed, _att.yawspeed);
+			
 			_actuators.control[CH_AIL] = _stabilization.getAileron();
 			_actuators.control[CH_ELV] = _stabilization.getElevator();
 			_actuators.control[CH_RDR] = _stabilization.getRudder();
 			_actuators.control[CH_THR] = _manual.throttle;
+
+			// XXX indirect stabilization
+			// _stabilization.update(
+			// _ratesCmd.roll, _ratesCmd.pitch, _ratesCmd.yaw,
+			// _att.rollspeed, _att.pitchspeed, _att.yawspeed);
+			// _actuators.control[CH_AIL] = _stabilization.getAileron();
+			// _actuators.control[CH_ELV] = _stabilization.getElevator();
+			// _actuators.control[CH_RDR] = _stabilization.getRudder();
+			// _actuators.control[CH_THR] = _manual.throttle;
 		}
 	}
 
